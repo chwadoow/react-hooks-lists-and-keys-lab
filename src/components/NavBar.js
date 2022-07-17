2b4e6cccbc5086c555ce9a8f51d827ff34a24b8b
moses ngige <moses32ngigz@gmail.com>
2022-07-17 05:44:25 +0300

    Complete

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,18 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
-}
+  // const tags = links.map((tag)=>{
+  //   <a key={tag} href={'#'+tag}></a>
 
+  // })
+
+  return (<nav>
+    <a key={links[0]} href='#home'>{links[0]}</a>
+    <a key={links[1]} href='#about'>{links[1]}</a>
+    <a key={links[2]} href='#projects'>{links[2]}</a>
+  
+ 
+  </nav>);
+
+}
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,28 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+  // console.log(projects);
+  const projectItems = projects.map((projo)=>(
+    <ProjectItem 
+      key={projo.id}
+      name={projo.name}
+      about={projo.about}
+      technologies={projo.technologies}
+    
+    />
+
+  ))
+
+
+
+
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+
+
+      <div id="project-list">{projectItems}</div>
     </div>
   );
 }
